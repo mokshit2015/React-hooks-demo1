128178ec7ce2ee531a52338b85875b5a4daa3c0f
mokshit2015 <mokshitshah2015@gmail.com>
2020-02-17 12:11:57 +0530

    Intial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const no1 = useRef(null);
+  const no2 = useRef(null);
+  const [no3,setNo] = useState(0);
+
+  const ButtonClick = () => {
+    no1.current.focus();
+  }
+
+  useEffect( () => {
+    no1.current.focus();
+  },[]); 
+
+  const SecondClick = () => {
+    no2.current.focus();
+    setNo(no3 + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input ref={no1} type="text"/>
+     <button onClick={ButtonClick}>Focus the input 1 </button>
+     <br></br>
+     <input ref={no2} type="text"/>
+     
+     <button onClick={SecondClick}>Focus and Update No</button>
+     <br></br>
+     No : {no3}
     </div>
   );
 }
